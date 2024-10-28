5bdb05cd5bde282f310bbeebca6dd4fccea3e33c
cyuanyuan520 <482734085@qq.com>
2024-10-28 22:03:10 +0800

    修改了test.java

--- src/com/itheima/main/Test.java
+++ src/com/itheima/main/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("helloWorld");
         System.out.println("吃大便区别");
+        System.out.println(5154);
     }
 }
